234a73216083c93b0c30d8b3de79066a57e23f39
akriti-thakur <yogyukta@gmail.com>
2025-01-22 12:48:50 +0530

    changes

--- hello.py
+++ hello.py
@@ -243,7 +243,12 @@
 
 
 
-# Given a positive integer A, return an array of strings with all the integers from 1 to N. But for multiples of 3 the array should have “Fizz” instead of the number. For the multiples of 5, the array should have “Buzz” instead of the number. For numbers which are multiple of 3 and 5 both, the array should have "FizzBuzz" instead of the number.
+# Given a positive integer A, return an array of strings with all the integers
+# from 1 to N. But for multiples of 3 the array should have “Fizz” 
+# instead of the number. For the multiples of 5, 
+# the array should have “Buzz” instead of the number. 
+# For numbers which are multiple of 3 and 5 both, the array should have 
+# "FizzBuzz" instead of the number.
 
 # def fizz_buzz(A):
 #     result = []
@@ -470,7 +475,6 @@
 
 # 	if (True):
 # 		count = 0
-
 # 		# checking small letters
 # 		for i in range(97, 123):
 
